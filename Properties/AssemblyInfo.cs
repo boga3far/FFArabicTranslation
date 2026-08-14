12963f8ef0f11d7aa4f92c2ae14e20bccc932380
ابراهيم بن احمد الحسن <214007514@student.kfu.edu.sa>
2026-08-14 19:49:57 +0300

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FFArabicTranslation")]
+[assembly: AssemblyDescription("Arabic localization mod for Farthest Frontier")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FFArabicTranslation")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8af5517e-0a9a-4ad2-ae79-d6f4a2f9f6a5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
